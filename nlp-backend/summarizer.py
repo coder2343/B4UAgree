22b11f61c9c465dd8316dd20d758e10ca0e3a02f
Liam Walsh <liamspencerwalsh@icloud.com>
2024-03-07 13:43:47 -0600

    add base functions and linter in file

--- nlp-backend/summarizer.py
+++ nlp-backend/summarizer.py
@@ -1,2 +1,16 @@
 class sumeraizer:
     pass
+
+
+    def receive_text(self):
+        pass
+
+
+    def html_To_Text(self,html):
+        pass
+
+    def get_Summary(self,text):
+        pass
+
+    def send_Summary(self):
+        pass
