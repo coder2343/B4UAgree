2a29a8ce826c0cf8ab52bd97a760d2f817c39709
Liam Walsh <liamspencerwalsh@icloud.com>
2024-04-03 18:38:13 -0500

    clean up contrroller to follow design practices
    controller just deals with send / recive text other functions are in other file.

--- nlp-backend/flask_controller.py
+++ nlp-backend/flask_controller.py
@@ -1,8 +1,6 @@
 import json
 from flask import Flask, jsonify, request
-
-from summarizer.sbert import SBertSummarizer
-from bs4 import BeautifulSoup, NavigableString, Tag
+from privacysummarizer import get_summary
 
 app = Flask(__name__)
 
@@ -26,34 +24,6 @@ def get_privacy_policy():
   print(PrivacyPolicy)
   return PrivacyPolicy
   
-
-
-# run methods here to ensure we can take care of 
-def html_to_text(html):
-    """Function take html file and extract given text."""
-    f = open("output/privacyPolicy.txt", "a", encoding="utf-8")
-    soup = BeautifulSoup(html, 'html.parser')
-    for header in soup.find_all({'h3','h2','h1'}):
-        next_node = header
-        while True:
-            next_node = next_node.nextSibling
-            if next_node is None:
-                break
-            if isinstance(next_node,NavigableString ):
-                print (next_node.strip())
-            if isinstance(next_node, Tag):
-                if next_node.name == "h2"or next_node.name == "h1" or next_node.name == "h3" :
-                    #print(soup.find(string=nextNode.text.strip()))
-                    break
-                f.write(next_node.get_text(strip=True).strip())
-    f.close()
-
-def get_summary(text,num_sentences):
-    """Function take text file and outputs given sumary of said text useing nlp model."""
-    model = SBertSummarizer('paraphrase-MiniLM-L6-v2')
-    result = model(text, num_sentences=num_sentences)
-    return result
-
 
 # get request 
 @app.route('/sum', methods=['GET'])
