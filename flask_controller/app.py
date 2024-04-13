8bbc880a58e2f9933e50e29232f946c01a615a98
Liam Walsh <liamspencerwalsh@icloud.com>
2024-04-13 13:53:58 -0500

    uncoment code

--- flask_controller/app.py
+++ flask_controller/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request
 import json
-from nlp_code import get_summary
 from summarizer.sbert import SBertSummarizer
 
 app = Flask(__name__)
@@ -16,18 +15,18 @@ def get_summary(text,num_sentences):
   result = model(text, num_sentences=num_sentences)
   return result
 
-# PrivacyPolicy= []
+PrivacyPolicy= []
 # # function gets privacy policy
-# @app.route('/sendpolicy', methods=['POST'])
-# def get_privacy_policy():
-#   request_data = request.get_json()
-#   retrivedPolicy= request_data['privacyPolicy']
-#   PrivacyPolicy.append(retrivedPolicy)
-#   print(PrivacyPolicy)
-#   return PrivacyPolicy
+@app.route('/sendpolicy', methods=['POST'])
+def get_privacy_policy():
+  request_data = request.get_json()
+  retrivedPolicy= request_data['privacyPolicy']
+  PrivacyPolicy.append(retrivedPolicy)
+  print(PrivacyPolicy)
+  return PrivacyPolicy
 
-# # get request 
-# @app.route('/sum', methods=['GET'])
-# def send_summary():
-#   make_sum= get_summary(PrivacyPolicy.pop(),5)
-#   return make_sum
+# get request 
+@app.route('/sum', methods=['GET'])
+def send_summary():
+  make_sum= get_summary(PrivacyPolicy.pop(),5)
+  return make_sum
