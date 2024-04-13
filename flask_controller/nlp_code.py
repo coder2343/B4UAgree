ccb2530e8648511a72dfd1d3e037dedb4080f06f
Liam Walsh <liamspencerwalsh@icloud.com>
2024-04-13 13:38:02 -0500

    make everything two file system

--- flask_controller/nlp_code.py
+++
@@ -1,8 +0,0 @@
-"""Module providing a function for sumerizing and bs4 is for webscrapeing."""
-from summarizer.sbert import SBertSummarizer
-
-def get_summary(text,num_sentences):
-    """Function take text file and outputs given sumary of said text useing nlp model."""
-    model = SBertSummarizer('paraphrase-MiniLM-L6-v2')
-    result = model(text, num_sentences=num_sentences)
-    return result
